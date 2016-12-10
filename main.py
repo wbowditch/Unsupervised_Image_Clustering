371c7286f2d629c6ccfb4e62202c57d395f809e6
Ryan Chan <chanrb@bc.edu>
2016-12-10 15:29:58 -0500

    new changes to main function to create arrays of images from command line args

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import sys
 
 def main(argv):
-    print "hello world"
-    for args in argv:
-        print argv
+    print argv
+    output_array = []
+    queryPath = argv[1]
+    databasePath = argv[2]
+    query_images = readImages(queryPath)
+    database_images = readImages(databasePath)
+    return output_array
 
 if __name__ == '__main__':
     main(sys.argv)
+
+#def readImages(filePath):
